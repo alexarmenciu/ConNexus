79cc104aa5dc00acf6d6837a88ec129e083ca61d
Potato455 <90859863+Potato455@users.noreply.github.com>
2023-04-05 19:26:41 -0400

    policy change

--- FrontEnd/frontend555/src/components/Policy.js
+++ FrontEnd/frontend555/src/components/Policy.js
@@ -1,69 +1,28 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
-// import UserService from "../services/user.service";
-
-// const Policy = () => {
-//   const [content, setContent] = useState("");
-
-//   useEffect(() => {
-//     UserService.getPublicContent().then(
-//       (response) => {
-//         setContent(response.data);
-//       },
-//       (error) => {
-//         const _content =
-//           (error.response && error.response.data) ||
-//           error.message ||
-//           error.toString();
-
-//         setContent(_content);
-//       }
-//     );
-//   }, []);
-
-//   return (
-//     <div className="container">
-//       <header className="jumbotron">
-//         <h3>Privacy Policy</h3>
-//       </header>
-//       <h2>
-//         - We only collect what information you give us. <br></br>
-//         - We will auto delete user accounts after one year of inactivity. <br></br>
-//         - We keep track of when users are last online to know when to auto delete accounts. <br></br>
-//         - We do not track anything else whatsoever. <br></br>
-//         - Users can modify all information provided.
-//       </h2>
-//     </div>
-//   );
-// };
-
-// export default Policy;
-
-
-import React, { Component } from 'react'
+import "./Policy.css";
 import ReactMarkdown from 'react-markdown'
 import termsFrPath from './PrivacyPolicy.md'
 
-class Policy extends Component {
-  constructor(props) {
-    super(props)
+const Policy = ()=> {
 
-    this.state = { terms: null }
-  }
+  let [ content, setContent] = useState({md: ""});
 
-  componentWillMount() {
-    fetch(termsFrPath).then((response) => response.text()).then((text) => {
-      this.setState({ terms: text })
-    })
-  }
+  useEffect(()=> {
+      fetch(termsFrPath)
+          .then((res) => res.text())
+          .then((md) => {
+              setContent({ md })
+          })
+  }, [])
 
-  render() {
-    return (
-      <div className="content">
-        <ReactMarkdown source={this.state.terms} />
-      </div>
-    )
-  }
+  return (
+    <div className="post">
+      <ReactMarkdown children={content.md}/>
+    </div>
+    
+  )
 }
 
-export default Policy
+export default Policy;
+
